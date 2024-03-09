df4dfba6c5535b5b94939365aea6c2b3da55c1fc
dsge3 <gerx03@gmail.com>
2024-03-09 10:09:58 +0100

    add supabase-js and stronger lint rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
+import { APP_SUPABASE_KEY, APP_SUPABASE_URL } from './app.config';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +13,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'racing-app';
+
+  constructor() {
+    const supabase: SupabaseClient = createClient(APP_SUPABASE_URL, APP_SUPABASE_KEY);
+    from(supabase.from('races').select().returns<object>()).subscribe((value: unknown) => {
+      console.log('races', value)
+    })
+  }
 }
