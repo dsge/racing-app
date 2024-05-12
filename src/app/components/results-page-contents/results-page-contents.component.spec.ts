30488d560236b4f0efad8173b3816701dd65453f
dsge3 <gerx03@gmail.com>
2024-05-12 11:17:01 +0200

    increase coverage

--- src/app/components/results-page-contents/results-page-contents.component.spec.ts
+++ src/app/components/results-page-contents/results-page-contents.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResultsPageContentsComponent } from './results-page-contents.component';
+import { of, take } from 'rxjs';
+import { Race, RaceScoreScreenVotes } from '../../models/race.model';
+import { UserVote } from '../../models/user-vote.model';
+import { Driver } from '../../models/driver.model';
 
 describe('ResultsPageContentsComponent', () => {
   let component: ResultsPageContentsComponent;
@@ -13,10 +17,108 @@ describe('ResultsPageContentsComponent', () => {
 
     fixture = TestBed.createComponent(ResultsPageContentsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should not fail when there is only a race passed', () => {
+      component.race$ = of({} as Race);
+      fixture.detectChanges();
+      expect(component.raceFinalResults$).toBeUndefined();
+    });
+
+    describe('raceFinalResults$', () => {
+      it('should be empty when there are no raceFinalResults', (done: DoneFn) => {
+        component.race$ = of({ id: 20001 } as Race);
+        component.raceScoreScreenVotes$ = of({
+          raceFinalResults: [],
+          userVotes: [],
+        } as RaceScoreScreenVotes);
+        fixture.detectChanges();
+        component.raceFinalResults$
+          .pipe(take(1))
+          .subscribe((finalResults?: UserVote[]) => {
+            expect(finalResults).toBeUndefined();
+            done();
+          });
+      });
+
+      it('should not be empty when there are raceFinalResults', (done: DoneFn) => {
+        component.race$ = of({ id: 20001 } as Race);
+        component.raceScoreScreenVotes$ = of({
+          raceFinalResults: [
+            {
+              driver_final_position: 5,
+              driver: {} as Driver,
+            },
+            {
+              driver_final_position: 2,
+              driver: {} as Driver,
+            },
+            {
+              driver_final_position: 2,
+              driver: {} as Driver,
+            },
+            {
+              is_fastest_lap_vote: true,
+              driver: {} as Driver,
+            },
+          ],
+          userVotes: [],
+        } as RaceScoreScreenVotes);
+        fixture.detectChanges();
+        component.raceFinalResults$
+          .pipe(take(1))
+          .subscribe((finalResults?: UserVote[]) => {
+            expect(finalResults).not.toBeUndefined();
+            done();
+          });
+      });
+    });
+  });
+
+  describe('getDriverForPosition', () => {
+    it('should return undefined when there are no drivers passed to the component', () => {
+      component.drivers = [];
+      expect(
+        component.getDriverForPosition(5, [
+          { is_fastest_lap_vote: false, driver_final_position: 5 },
+        ])
+      ).toBeUndefined();
+    });
+
+    it('should return the driver when there are drivers passed to the component', () => {
+      component.drivers = [
+        {
+          id: 8,
+          full_name: 'foo',
+          year_of_racing: 2015,
+        },
+      ];
+      expect(
+        component.getDriverForPosition(5, [
+          { driver_final_position: 5, driver_id: 8 },
+        ])?.full_name
+      ).toBe('foo');
+    });
+
+    it('should return the driver for fastest lap vote', () => {
+      component.drivers = [
+        {
+          id: 8,
+          full_name: 'foo',
+          year_of_racing: 2015,
+        },
+      ];
+      expect(
+        component.getDriverForPosition('fastest-lap', [
+          { is_fastest_lap_vote: true, driver_id: 8 },
+        ])?.full_name
+      ).toBe('foo');
+    });
   });
 });
--- src/app/components/sidebar/sidebar.component.spec.ts
+++ src/app/components/sidebar/sidebar.component.spec.ts
@@ -2,10 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SidebarComponent } from './sidebar.component';
 import { provideMockSupabaseClient } from '../../providers/provide-mock-supabase-client';
+import { of, take } from 'rxjs';
+import { MenuItem, PrimeIcons } from 'primeng/api';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
+  let router: Router;
+  let userService: UserService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,10 +21,36 @@ describe('SidebarComponent', () => {
 
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    router = TestBed.inject(Router);
+    userService = TestBed.inject(UserService);
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  describe('items$', () => {
+    it('should return some items', (done: DoneFn) => {
+      fixture.detectChanges();
+      component.items$.pipe(take(1)).subscribe((items: MenuItem[]) => {
+        expect(items.length).toBeGreaterThan(0);
+        done();
+      });
+    });
+
+    it('should navigate on logout click', (done: DoneFn) => {
+      router.navigate = jasmine.createSpy();
+      userService.signOut = jasmine.createSpy().and.returnValue(of(null));
+      fixture.detectChanges();
+      component.items$.pipe(take(1)).subscribe((items: MenuItem[]) => {
+        const logoutMenuItem: MenuItem = items.find(
+          (item: MenuItem) => item.icon === PrimeIcons.SIGN_OUT
+        )!;
+        logoutMenuItem.command?.({});
+        expect(router.navigate).toHaveBeenCalledTimes(1);
+        done();
+      });
+    });
   });
 });
